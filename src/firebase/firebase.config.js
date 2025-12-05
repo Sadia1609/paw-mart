3b046d47cf164a2722a0718e89c01545bf3a27aa
Sadia1609 <sadia.rahman160920@gmail.com>
2025-12-05 21:02:55 +0600

    added firebase and node modules

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID 
+  apiKey: "AIzaSyBLsoq7MLCOJEbqfPJpzZmvf4LzHgDraWM",
+  authDomain: "paw-mart-4995d.firebaseapp.com",
+  projectId: "paw-mart-4995d",
+  storageBucket: "paw-mart-4995d.firebasestorage.app",
+  messagingSenderId: "170199557938",
+  appId: "1:170199557938:web:4d4ef4d83159656426192b"
 };
 
 
