128ca0bdac2fe58e84688278a52cf8d291ff13ae
Sadia1609 <sadia.rahman160920@gmail.com>
2025-12-07 15:09:27 +0600

    toggle theme added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router'; 
 import { AuthContext } from '../Provider/AuthProvider';
 import { signOut } from 'firebase/auth';
@@ -6,6 +6,26 @@ import auth from '../firebase/firebase.config';
 
 const Navbar = () => {
   const { user } = useContext(AuthContext);
+ const [isChecked, setIsChecked] = useState(() => {
+  const savedTheme = localStorage.getItem("theme");
+  return savedTheme === "dark";
+});
+
+
+
+  //toggle
+ 
+   useEffect(() => {
+  const theme = isChecked ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", theme);
+
+  localStorage.setItem("theme", theme);
+}, [isChecked]);
+
+ const handleThemeChange = ()=>{
+    setIsChecked(prev => !prev);
+  }
+
 
   const handleSignout = () => {
     signOut(auth);
@@ -94,6 +114,7 @@ const Navbar = () => {
         </ul>
       </div>
 
+
       <div className="navbar-end">
         {user ? (
           <button onClick={handleSignout} className="btn">
@@ -105,6 +126,42 @@ const Navbar = () => {
           </Link>
         )}
       </div>
+
+
+      
+         <div className='navbar-end'>
+           <label className="flex cursor-pointer gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <circle cx="12" cy="12" r="5" />
+    <path
+      d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+  </svg>
+  <input onClick={handleThemeChange} checked={isChecked} type="checkbox" value="synthwave" className="toggle theme-controller" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+  </svg>
+</label>
+
+         </div>
+
     </div>
   );
 };
